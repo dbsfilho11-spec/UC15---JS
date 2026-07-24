418c7bae76ce9c6aaef1b733ab5b58379b9446b2
dbsfilho11-spec <dbsfilho11@gmail.com>
2026-07-24 18:51:21 -0300

    Add files via upload

---
+++ UC15-JS/script.js
@@ -0,0 +1,32 @@
+console.log("Entregue a chama ao governante")
+console.error("Chama falsa")
+console.warn("Saudem o mestre Grimm")
+
+//let nome = "Grimm"
+let Idade = "Desconhecida"
+let Almas = 500*3
+console.log(Almas + " Almas coletadas")
+//console.log(nome)
+console.log("Idade " + Idade)
+console.log(1500 > 10000)
+
+alert("ATENÇÃO")
+confirm("Você deseja continuar?")
+
+let nome = prompt("Qual seu nome?")
+console.log("Olá" + nome)
+
+let cor = prompt("Qual sua cor favorita")
+console.log("Sua cor favorita é " + cor)
+
+if (nome == "Daniel") {
+    alert ("Aprovado")
+} else {
+    alert("Reprovado")
+}
+
+if (cor == "Orange") {
+    alert ("Aprovado chefia")
+} else {
+    alert ("Quem é você pae?")
+} 
