251cb629b4163adb8d3b239d3553434986fdc637
dbsfilho11-spec <dbsfilho11@gmail.com>
2026-08-07 17:00:21 -0300

    Add files via upload

--- ATIVIDADE UC-15IMC/Script.js
+++ ATIVIDADE UC-15IMC/Script.js
@@ -1,20 +1,21 @@
 let nome = prompt('Qual seu nickname boy?')
-let nota1 = prompt('Qual sua nota na AV1 boy?')
-let nota2 = prompt('Qual sua nota na AV2 boy?')
-let nota3 = prompt('Qual sua nota na AV3 boy?')
+let nota1 = Number(prompt('Qual sua nota na AV1 boy?'))
+let nota2 = Number(prompt('Qual sua nota na AV2 boy?'))
+let nota3 = Number(prompt('Qual sua nota na AV3 boy?'))
 
 let NotaF = (nota1 + nota2 + nota3) / 3
+NotaF = Number(NotaF.toFixed(1))
+let NotaFF = NotaF.toFixed(1)
+
+if (NotaF < 4) {
+    alert('Reprovado')
+} else if (5 < NotaF && NotaF < 6) {
+    alert('Recuperação')
+} else if (NotaF > 7) {
+    alert('Aprovado')
+}
 
 alert(`${nome} tirou ${NotaF}`)
 console.log(`${nome} tirou ${NotaF}`)
 
-if (NotaF < 4) {
-    alert(Reprovado)
-} else if (5 < NotaF < 6) {
-    alert(Recuperação)
-} else if  (7 < NotaF == 10) {
-    alert(Aprovado)
-} else if (NotaF > 10) {
-    alert(Aprovado)
-}
 
